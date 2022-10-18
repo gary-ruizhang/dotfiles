af72d5f52a42041e16dc0c38044171f0ba1c9f65
ruizhang <gary.ruizhang@gmail.com>
2022-10-18 17:08:17 +0800

    change terminal from alacritty to wezterm, TODO quit wezterm will also detach current tmux window

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,24 @@
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window{}
+  -- window:gui_window():maximize()
+  window:gui_window():toggle_fullscreen()
+end)
+
+return {
+  -- ui
+  color_scheme = 'nordfox',
+  font = wezterm.font 'OperatorMonoSSmLig Nerd Font',
+  font_size = 20,
+
+  -- window
+  window_close_confirmation = "NeverPrompt",
+  window_decorations = "RESIZE",
+  enable_tab_bar = false,
+  window_padding = {
+    left = 0,
+    bottom = 0,
+  },
+}
