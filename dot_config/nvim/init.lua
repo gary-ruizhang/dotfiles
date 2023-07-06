37bf138eb145d5dccba1e9a0295bcc872302381a
ruizhang <gary.ruizhang@gmail.com>
2023-07-06 15:29:30 +0800

    customize lazyvim config, change font to JetBrains Mono

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local actions = require("telescope.actions")
+
+require("telescope").setup({
+  defaults = {
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+      },
+    },
+  },
+})
