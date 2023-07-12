80466d61b943374eb8d65d24110196340bc7e022
ruizhang <gary.ruizhang@gmail.com>
2023-07-12 15:01:56 +0800

    change alpha welcome page new file keymap, change mini.surround keymap to vim.surround

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -1,3 +1,14 @@
 return {
   { "echasnovski/mini.align", version = false },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "ys",
+        delete = "ds",
+        replace = "cs",
+      },
+      search_method = 'cover_or_next',
+    },
+  }
 }
