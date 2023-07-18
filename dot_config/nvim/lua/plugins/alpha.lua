abc0af4140c40b582d2d7e70177b9d262d804ddd
ruizhang <gary.ruizhang@gmail.com>
2023-07-18 08:41:57 +0800

    tune tmux apperance

--- dot_config/nvim/lua/plugins/alpha.lua
+++ dot_config/nvim/lua/plugins/alpha.lua
@@ -15,7 +15,8 @@ return {
       dashboard.section.header.val = vim.split(logo, "\n")
       dashboard.section.buttons.val = {
         dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-        dashboard.button("i", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+        -- dashboard.button("i", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+        dashboard.button("i", " " .. " New file", ":ene<CR>"),
         dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
         dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
         dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -11,7 +11,20 @@ return {
 	-- ui
 	color_scheme = "nordfox",
 	font = wezterm.font("OperatorMonoSSmLig Nerd Font Mono"),
-	font_size = 14,
+	font_size = 20,
+
+  window_background_image = "/Users/ruizhang/FUCK/nordfox.jpeg",
+  window_background_image_hsb = {
+    -- Darken the background image by reducing it to 1/3rd
+    brightness = 0.3,
+
+    -- You can adjust the hue by scaling its value.
+    -- a multiplier of 1.0 leaves the value unchanged.
+    hue = 1.0,
+
+    -- You can adjust the saturation also.
+    saturation = 1.0,
+  },
 
 	-- window
 	window_close_confirmation = "NeverPrompt",
