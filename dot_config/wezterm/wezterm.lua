1d44991e161062b369b94398d67715c6faf9be94
ruizhang <gary.ruizhang@gmail.com>
2023-08-06 22:50:18 +0800

    change wezterm to default terminal, port dracula pro theme from alacritty to wezterm. TODO check the wezterm mux feature, and maybe switch from tmux in the future

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -9,29 +9,34 @@ end)
 
 return {
 	-- ui
-	color_scheme = "nordfox",
-	font = wezterm.font("MonoLisaStaticScript Nerd Font"),
+	color_scheme = "dracula-pro",
+	font = wezterm.font("MonoLisa Static-Script"),
 	font_size = 20,
 
-  window_background_image = "/Users/ruizhang/FUCK/nordfox.jpeg",
-  window_background_image_hsb = {
-    -- Darken the background image by reducing it to 1/3rd
-    brightness = 0.3,
+  -- window_background_image = "/Users/ruizhang/FUCK/nordfox.jpeg",
+  -- window_background_image_hsb = {
+  --   -- Darken the background image by reducing it to 1/3rd
+  --   brightness = 0.3,
+  --
+  --   -- You can adjust the hue by scaling its value.
+  --   -- a multiplier of 1.0 leaves the value unchanged.
+  --   hue = 1.0,
+  --
+  --   -- You can adjust the saturation also.
+  --   saturation = 1.0,
+  -- },
 
-    -- You can adjust the hue by scaling its value.
-    -- a multiplier of 1.0 leaves the value unchanged.
-    hue = 1.0,
-
-    -- You can adjust the saturation also.
-    saturation = 1.0,
-  },
-
-	-- window
 	window_close_confirmation = "NeverPrompt",
 	window_decorations = "RESIZE",
 	enable_tab_bar = false,
 	window_padding = {
+    top = 0,
 		left = 0,
 		bottom = 0,
 	},
+
+  -- color
+
+  -- utilities
+  audible_bell = "Disabled",
 }
